c62e2d3d4d31d1665b6977c9eb66ebcea3f21c91
Nono2 <Nono2@DESKTOP-OVN5N6P>
2021-02-22 00:43:59 +0200

    Initial window

---
+++ Test2/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test2
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			InitTwitchTab();
+		}
+
+		private void InitTwitchTab()
+		{
+			TwitchTabHandleAgreed();
+        }
+
+		private void twitch_user_agreed_Clicked(object sender, RoutedEventArgs e)
+		{
+			TwitchTabHandleAgreed();
+		}
+
+
+        void TwitchTabHandleAgreed()
+        {
+            bool agreed = twitch_user_agreed.IsChecked.GetValueOrDefault();
+
+            twitch_user_channel_name_label.IsEnabled = agreed;
+            twitch_user_channel_name.IsEnabled = agreed;
+            twitch_user_channel_oauth_label.IsEnabled = agreed;
+            twitch_user_channel_oauth.IsEnabled = agreed;
+            twitch_user_user_name_label.IsEnabled = agreed;
+            twitch_user_user_name.IsEnabled = agreed;
+        }
+
+		private void OnlyNumbersPreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			Utils.HandleOnlyNumbersPreviewTextInput(e);
+		}
+
+
+		private void NoSpacePreviewKeyDown(object sender, KeyEventArgs e)
+		{
+			Utils.HandleNoSpacePreviewKeyDown(e);
+		}
+
+		private void NoCopyPastePreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			Utils.HandleNoCopyPastePreviewExecuted(e);
+		}
+	}
+}
---
+++ Test2/Utils.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+
+namespace Test2
+{
+	internal class Utils
+	{
+
+        public static void HandleOnlyNumbersPreviewTextInput(TextCompositionEventArgs e)
+        {
+            if (e.Text.Length == 0 || !char.IsDigit(e.Text[0]))
+            {
+                e.Handled = true;
+            }
+        }
+
+        public static void HandleNoSpacePreviewKeyDown(KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+
+        public static void HandleNoCopyPastePreviewExecuted(ExecutedRoutedEventArgs e)
+        {
+            if (e.Command == ApplicationCommands.Copy || e.Command == ApplicationCommands.Cut || e.Command == ApplicationCommands.Paste)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
