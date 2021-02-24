4fe1d2f2c8940590c88c0c6224ee14119b023947
Nono2 <Nono2@DESKTOP-OVN5N6P>
2021-02-24 22:30:05 +0200

    options file

--- Test2/MainWindow.xaml.cs
+++ Test2/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Test2
 {
@@ -20,6 +8,10 @@ namespace Test2
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+
+		private OptionsFile twitchFile = new OptionsFile("twitch.ini");
+
+
 		public MainWindow()
 		{
 			InitializeComponent();
---
+++ Test2/OptionsFile.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Test2
+{
+
+    internal class Options
+    {
+        [XmlElement("enableTwitchVoting")]
+        public string EnableTwitchVoting;
+        [XmlElement("twitchChannelName")]
+        public string TwitchChannelName;
+        [XmlElement("twitchUserName")]
+        public string TwitchUserName;
+        [XmlElement("oAuth")]
+        public string OAuth;
+    }
+
+
+
+    internal class OptionsFile
+	{
+		private string fileName;
+        private Options options;
+
+
+		public OptionsFile(string fileName)
+		{
+			this.fileName = fileName;
+		}
+
+        public void ReadFile()
+        {
+            //TODO  see if it works
+
+            // Create an instance of the XmlSerializer.
+            XmlSerializer serializer = new XmlSerializer(typeof(Options));
+
+            // Declare an object variable of the type to be deserialized.
+            using (Stream reader = new FileStream(fileName + ".xml", FileMode.Open))
+            {
+                // Call the Deserialize method to restore the object's state.
+                this.options = (Options)serializer.Deserialize(reader);
+            }
+
+        }
+
+
+        public void WriteFile()
+        {
+            //TODO  see if it works
+
+            XmlSerializer serilizer = new XmlSerializer(this.options.GetType());
+            serilizer.Serialize(Console.Out, this.options);
+        }
+    }
+}
